c6ca0abb3fbe84936d1ee4c1ac3cca087b73b3a4
Paul Tomchik <pauljtomchik@gmail.com>
2022-01-15 15:07:39 -0500

    DDL to create the data_manager sources and views tables.

---
+++ sql/sources/create_sources_table.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS data_manager ;
+
+CREATE TABLE IF NOT EXISTS data_manager.sources (
+  id                SERIAL PRIMARY KEY,
+
+  name              TEXT NOT NULL UNIQUE,
+  update_interval   TEXT[],
+  category          TEXT,
+  description       TEXT,
+  statistics        JSONB,
+  metadata          JSONB
+) ;
+
+COMMIT ;
---
+++ sql/views/create_views_table.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS data_manager ;
+
+CREATE TABLE IF NOT EXISTS data_manager.views (
+  id                  SERIAL PRIMARY KEY,
+
+  source_id           INTEGER NOT NULL
+                        REFERENCES data_manager.sources (id)
+                        ON DELETE CASCADE,
+  data_type           TEXT,
+  interval_version    TEXT, -- could be year, or year-month
+  geography_version   TEXT, -- mostly 2 digit state codes, sometimes null
+  version             TEXT, -- default 1
+  source_url          TEXT, -- external source url
+  publisher           TEXT,
+  data_table          TEXT, -- schema.table of internal destination
+  download_url        TEXT, -- url for client download
+  tiles_url           TEXT, -- tiles
+  start_date          DATE,
+  end_date            DATE,
+  last_updated        DATE,
+  statistics          JSONB
+) ;
+
+COMMIT ;
